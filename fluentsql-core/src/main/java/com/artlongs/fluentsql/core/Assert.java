81d65bbfc57035129de5931fdf3e546ddb37853e
liqingfeng <liqf@pvc123.com>
2019-06-26 14:51:14 +0800

    FIX:ASSERT

--- fluentsql-core/src/main/java/com/artlongs/fluentsql/core/Assert.java
+++ fluentsql-core/src/main/java/com/artlongs/fluentsql/core/Assert.java
@@ -1,33 +1,33 @@
 package com.artlongs.fluentsql.core;
 
 /**
- * Func :
+ * Func : trueThenThrow
  *
  * @author: leeton on 2019/6/21.
  */
 public class Assert {
     public static void isNull(Object val, String throwMsg) {
-        that(null == val, throwMsg);
+        trueThenThrow(null == val, throwMsg);
     }
 
     public static void isBlank(Object val, String throwMsg) {
-        that(null == val || String.valueOf(val).trim().length() == 0, throwMsg);
+        trueThenThrow(null == val || String.valueOf(val).trim().length() == 0, throwMsg);
     }
 
     public static void isNotFound(Object val, String throwMsg) {
-        that(null == val || val.equals("false") || val.equals(-1), throwMsg);
+        trueThenThrow(null == val || val.equals("false") || val.equals(-1), throwMsg);
     }
 
     public static void isFalse(boolean test, String throwMsg) {
-        that(test, throwMsg);
+        trueThenThrow(false==test, throwMsg);
     }
 
     public static void isTrue(boolean test, String throwMsg) {
-        that(false==test, throwMsg);
+        trueThenThrow(test, throwMsg);
     }
 
-    public static void that(boolean test, String throwMsg) {
-        if (!test) {
+    public static void trueThenThrow(boolean test, String throwMsg) {
+        if (test) {
             throw new RuntimeException(throwMsg);
         }
     }
